3bc00c0b0230db943dd901a6270bee828db48a9d
Boris Bonilla <borisb1@gmail.com>
2018-08-05 14:28:45 -0300

    Get the location image from API

--- src/js/app.js
+++ src/js/app.js
@@ -4,25 +4,22 @@ var markers = [];
 // Set the default locations that will be displayed in the map
 var locations = [
     {
-        title: 'Jardín Botánico Carlos Thays',
+        title: 'Buenos Aires Botanical Garden',
+        originalName:'Jardín Botánico Carlos Thays',
         location: {lat: -34.582576,lng: -58.417388},
         image : null,
         description: null
     },
     {
-        title: 'Plaza Serrano',
-        location: {lat: -34.5876842,lng: -58.4299145},
-        image : null,
-        description: null
-    },
-    {
-        title: 'Museo de Arte Latinoamericano',
+        title: 'MALBA',
+        originalName:'MALBA',
         location: {lat: -34.577012,lng: -58.403330},
         image : null,
         description: null
     },
     {
-        title: 'Museo Nacional de Arte Decorativo',
+        title: 'National Museum of Decorative Arts',
+        originalName:'Museo Nacional de Bellas Artes',
         location: {lat: -34.583780, lng: -58.392342},
         image : null,
         description: null
@@ -68,11 +65,16 @@ var initMap = function() {
         // Check if is not already open
         if(marker != infoWindow.marker) {
 
+            // Set the image path if was already getted from API
+            var imageUrl = locations[marker.id]['image'] !== null ?
+                locations[marker.id]['image'] :
+                'images/blank.png';
+
             // Set the tile and base elements in the infowindow
             infoWindow.setContent(`
                 <div class="info-container">
                     <h2>`+marker.title+`</h2>
-                    <img src="images/blank.png" id="location-image">
+                    <img src="`+imageUrl+`" id="location-image" alt="`+marker.title+`">
                     <p id="location-description">Loading description...</p>
                 </div>
             `);
@@ -83,12 +85,60 @@ var initMap = function() {
                 infoWindow.marker = null;
             })
 
-            // Get the information that will be displayed from the Fousquare API
-
+            // Check if the location item has an image otherwise
+            // get the information from the Foursquare API
+            if(locations[marker.id]['image'] === null) {
+                var markerLocation = locations[marker.id].location;
+                $.ajax({
+                    url: 'https://api.foursquare.com/v2/venues/search?'+
+                            'll='+markerLocation.lat+','+markerLocation.lng+'&'+
+                            'client_id=KOO1EUSAY50U1VPWO4S51XAQQQWBEDSC2VMUGBYER25AW5T2&'+
+                            'client_secret=M04BBX5TH4EWRN22OPIPYLYZBO0LHFJS0CGJXRD1EBMH4DOY&'+
+                            'v=20180801&'+
+                            'query='+locations[marker.id]['originalName'],
+                    dataType: 'json',
+                    success: function(data) {
+                        // Get the picture using the venue id received
+                        getPicture(data.response.venues[0].id, marker.id);
+                    },
+                    error: function() {
+                        return false;
+                    }
+                });
+            }
 
             // Open the infowindow
             infoWindow.open(map, marker);
         }
+    }
+
+    // This function will return the picture url from Foursquare API
+    // using the venue id provided, then set the url into the img tag and
+    // save the result url in the location object
+    var getPicture = function(venueId, locationId) {
+        $.ajax({
+            url: 'https://api.foursquare.com/v2/venues/'+venueId+'/photos?'+
+                    'client_id=KOO1EUSAY50U1VPWO4S51XAQQQWBEDSC2VMUGBYER25AW5T2&'+
+                    'client_secret=M04BBX5TH4EWRN22OPIPYLYZBO0LHFJS0CGJXRD1EBMH4DOY&'+
+                    'v=20180801',
+            dataType: 'json',
+            success: function(data) {
+                var venuePhoto = data.response.photos.items[0];
+
+                // Set the picture in the infowindow img tag
+                var locationUrl = venuePhoto != undefined ?
+                    venuePhoto.prefix+'200x100'+venuePhoto.suffix : null;
+                $('#location-image').attr('src',
+                    locationUrl != null ? locationUrl : 'images/blank.png'
+                );
+
+                // Save the url in the location object
+                locations[locationId]['image'] = locationUrl;
+            },
+            error: function() {
+                return false;
+            }
+        });
     }
 }
 
